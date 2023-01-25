c4870d9647211efe1411b4938ca27dcfb3ef7050
Joao Lourenco <joaodslourenco@gmail.com>
2023-01-25 09:30:49 -0300

    feat: update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,13 +11,28 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
-      flash[:notice] = "Article was created successfully."
+      flash[:notice] = 'Article was created successfully.'
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    updated_article = @article.update(params.require(:article).permit(:title, :description))
+    if updated_article
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 end
